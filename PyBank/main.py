e9a35341670d530b3b803ea232fe3cc2a6b3277e
sunnykskim <51854898+sunnykskim@users.noreply.github.com>
2019-08-03 16:51:39 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,21 +11,20 @@ import csv
 #Set Path for File
 csvpath = os.path.join("..","budget_data.csv")
 
-#set list
-months = [] 
-totalrevchange = []
-
-#provide value to variables
-totalpl = 0
-prevrev = 0 
-revchange = 0 
-toprevincrease = [0,0]
-toprevdecrease = [0,0]
-
-
 #CSV Module
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile,delimiter=',')
+
+    #set list
+    months = []
+    MoMdelta = [] 
+
+    #provide value to variables
+    totalpl = 0
+    pastrev = 0
+    highestinc = 0
+    highestdec = 999999999
+    totalmomdelta = 0
     
     #skip header
     next(csvreader, None)
@@ -35,29 +34,33 @@ with open(csvpath, newline='') as csvfile:
         months.append(row[0])
         #add up all the profits and loss in row 1/column b
         totalpl = totalpl + int(row[1])
-
-        #max & min change
-        revchange = int(row[1]) - int(prevrev)
-        if len(totalrevchange) > 0:
-            if revchange > int(toprevincrease[1]):
-                toprevincrease = [row[0],revchange]
-                
-            if revchange < int(toprevdecrease[1]):
-                toprevdecrease = [row[0],revchange]
-                
-        
-        totalrevchange.append(revchange)
-        prevrev = row[1]
-
+        #create a list of MoM change
+        revenuechange = int(row[1]) - pastrev
+        pastrev = int(row[1])
+        MoMdelta.append(revenuechange)
+        #dont forget to calculate average avg of MoMdelta outside 
+        #greatestincrease
+        if (revenuechange > highestinc):
+            highestincdate = row[0]
+            highestinc = revenuechange
+        #greatestdecrease
+        if (revenuechange < highestdec):
+            highestdecdate = row [0]
+            highestdec = revenuechange
+    #calculate average of MoMdelta        
+    del MoMdelta[0]
+    avgmomdelta = sum(MoMdelta)/(len(months) - 1)
     
 #print
 totalmonths = len(months)
 print(totalmonths)
 print(totalpl)
-print(toprevdecrease)
-print(toprevincrease)
-
-
+print(avgmomdelta)
+print(highestinc)
+print(highestincdate)
+print(highestdec)
+print(highestdecdate)
+print(MoMdelta)
 
 
 
